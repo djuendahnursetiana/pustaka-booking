64ae0f68b941479cc1b3580c708a58682058be59
djuendahnursetiana <juendah@gmail.com>
2022-06-03 20:51:12 +0700

    .

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -26,4 +26,88 @@ class Laporan extends CI_Controller
 
         $this->load->view('buku/laporan_print_buku', $data);
     }
+
+    public function laporan_buku_pdf()
+    {
+
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+
+        $this->load->library('pdfgenerator');
+        $this->load->view('buku/laporan_pdf_buku', $data);
+
+        // title dari pdf
+        $this->data['title_pdf'] = 'Laporan Penjualan Toko Kita';
+
+        // filename dari pdf ketika didownload
+        $file_pdf = 'laporan_penjualan_toko_kita';
+        // setting paper
+        $paper = 'A4';
+        //orientasi paper potrait / landscape
+        $orientation = "portrait";
+
+        $html = $this->output->get_output();
+
+        // run dompdf
+        $this->pdfgenerator->generate($html, $file_pdf, $paper, $orientation);
+    }
+
+    public function export_excel()
+    {
+        $data = [
+            'title' => 'Laporan Buku',
+            'buku' => $this->ModelBuku->getBuku()->result_array()
+        ];
+        $this->load->view('buku/export_excel_buku', $data);
+    }
+
+    public function laporan_pinjam()
+    {
+        $data['judul'] = 'Laporan Data Peminjaman';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['laporan'] = $this->db->query("select * from pinjam p,detail_pinjam d,buku b,user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('pinjam/laporan-pinjam', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function cetak_laporan_pinjam()
+    {
+        $data['laporan'] = $this->db->query("select * from pinjam p,detail_pinjam d,buku b,user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
+        $this->load->view('pinjam/laporan-print-pinjam', $data);
+    }
+
+    public function laporan_pinjam_pdf()
+    {
+        $data['laporan'] = $this->db->query("select * from pinjam p,detail_pinjam d,buku b,user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
+
+        $this->load->library('pdfgenerator');
+        $this->load->view('pinjam/laporan-pdf-pinjam', $data);
+
+        // title dari pdf
+        $this->data['title_pdf'] = 'Laporan Penjualan Toko Kita';
+
+        // filename dari pdf ketika didownload
+        $file_pdf = 'laporan_penjualan_toko_kita';
+        // setting paper
+        $paper = 'A4';
+        //orientasi paper potrait / landscape
+        $orientation = "portrait";
+
+        $html = $this->output->get_output();
+
+        // run dompdf
+        $this->pdfgenerator->generate($html, $file_pdf, $paper, $orientation);
+    }
+
+    public function export_excel_pinjam()
+    {
+        $data = array(
+            'title' => 'Laporan Data Peminjaman Buku',
+            'laporan' => $this->db->query("select * from pinjam p,detail_pinjam d,buku b,user u where d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array()
+        );
+        $this->load->view('pinjam/export-excel-pinjam', $data);
+    }
 }
+
